50e2d915963ebfe0882169295e5f06d4cebe0a94
davilivob <david.iii.chiu@gmail.com>
2023-09-14 20:59:59 +0800

    Abstract Factory (improved)

--- creational/AbstractFactory.ts
+++ creational/AbstractFactory.ts
@@ -5,81 +5,80 @@
 // Function --> Func
 // Collaborator --> Coll
 
-interface AbsFact {
-	create_prod_A(): AbsProdA;
-	create_prod_B(): AbsProdB;
+interface IFact {
+	create_Prod_A(): IProdA;
+	create_Prod_B(): IProdB;
 }
 
-interface AbsProdA {
+interface IProdA {
 	func(): string;
 }
-interface AbsProdB {
+interface IProdB {
 	func(): string;
-	another_func(coll: AbsProdA): string;
+	another_func(coll: IProdA): string;
 }
 
-class ConFact1 implements AbsFact {
-	public create_prod_A(): AbsProdA {
-		return new ConProdA1();
+class Fact1 implements IFact {
+	public create_Prod_A(): IProdA {
+		return new ProdA1();
 	}
-	public create_prod_B(): AbsProdB {
-		return new ConProdB1();
-	}
-}
-class ConFact2 implements AbsFact {
-	public create_prod_A(): AbsProdA {
-		return new ConProdA2();
-	}
-	public create_prod_B(): AbsProdB {
-		return new ConProdB2();
+	public create_Prod_B(): IProdB {
+		return new ProdB1();
 	}
 }
 
-class ConProdA1 implements AbsProdA {
-	public func() {
+class Fact2 implements IFact {
+	public create_Prod_A(): IProdA {
+		return new ProdA2();
+	}
+
+	public create_Prod_B(): IProdB {
+		return new ProdB2();
+	}
+}
+
+class ProdA1 implements IProdA {
+	public func(): string {
 		return 'the result of A1';
 	}
 }
 
-class ConProdA2 implements AbsProdA {
-	public func() {
+class ProdA2 implements IProdA {
+	public func(): string {
 		return 'the result of A2';
 	}
 }
 
-class ConProdB1 implements AbsProdB {
-	public func() {
+class ProdB1 implements IProdB {
+	public func(): string {
 		return 'the result of B1';
 	}
-
-	public another_func(coll: AbsProdA): string {
+	public another_func(coll: IProdA): string {
 		const result = coll.func();
 		return `the result of B1 work with (${result})`;
 	}
 }
-class ConProdB2 implements AbsProdB {
-	public func() {
+
+class ProdB2 implements IProdB {
+	public func(): string {
 		return 'the result of B2';
 	}
-
-	public another_func(coll: AbsProdA): string {
+	public another_func(coll: IProdA): string {
 		const result = coll.func();
 		return `the result of B2 work with (${result})`;
 	}
 }
 
-function client_code(fact: AbsFact) {
-	const p_A = fact.create_prod_A();
-	const p_B = fact.create_prod_B();
+function client_code(fact: IFact) {
+	const pA = fact.create_Prod_A();
+	const pB = fact.create_Prod_B();
 
-	console.log(p_B.func());
-	console.log(p_B.another_func(p_A));
+	console.log(pB.func());
+	console.log(pB.another_func(pA));
 }
 
 console.log();
-console.log('F1 test');
-client_code(new ConFact1());
+client_code(new Fact1());
 console.log();
-console.log('F2 test');
-client_code(new ConFact2());
+client_code(new Fact2());
 console.log();
