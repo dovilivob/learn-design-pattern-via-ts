f8b4338a41833e92873013f26f50010ff70c863c
davilivob <david.iii.chiu@gmail.com>
2023-09-16 16:31:55 +0800

    Chain of Responsibility

---
+++ src/Behavioral/ChainOfResponsibility.ts
@@ -0,0 +1,62 @@
+interface Handler {
+	set_next(handler: Handler): Handler;
+	handle(req: string): string | null;
+}
+
+abstract class Handler implements Handler {
+	private next: Handler | undefined;
+
+	public set_next(handler: Handler): Handler {
+		this.next = handler;
+		return handler;
+	}
+
+	public handle(req: string): string | null {
+		if (this.next) return this.next.handle(req);
+		return null;
+	}
+}
+
+class Monke extends Handler {
+	public handle(req: string): string | null {
+		if (req === '🍌') return `🦍: "Thx bruh. I'ma eat that ${req}."`;
+		return super.handle(req);
+	}
+}
+class Snake extends Handler {
+	public handle(req: string): string | null {
+		if (req === '🐁') return `🐍: "Ah... I'ma eat that ${req}."`;
+		return super.handle(req);
+	}
+}
+class Cat extends Handler {
+	public handle(req: string): string | null {
+		if (req === '🪳') return `🐈: "Meow meow. I'ma fuck that ${req}."`;
+		return super.handle(req);
+	}
+}
+
+function client_code(handler: Handler) {
+	console.log();
+	const foods = ['🍌', '🥩', '🪳', '🍎', '🐁'];
+	for (const food of foods) {
+		console.log(`😀: "Who wants this ${food}?"`);
+		const result = handler.handle(food);
+		if (result) console.log(result);
+		else console.log('(Wow, nobody likes that garbage...)');
+		console.log();
+	}
+}
+
+const monke = new Monke();
+const snake = new Snake();
+const cat = new Cat();
+
+monke.set_next(snake).set_next(cat);
+
+console.log('🦍 --> 🐍 --> 🐈');
+client_code(monke);
+console.log('🐍 --> 🐈');
+client_code(snake);
+console.log('🐈');
+client_code(cat);
